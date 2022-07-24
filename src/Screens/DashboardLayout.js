fb74b6822e8146569241e2755d8404f58dabb77e
Sharar Nur <shararnur@gmail.com>
2022-07-24 19:26:37 +0600

    Added navbar and font family

--- src/Screens/DashboardLayout.js
+++ src/Screens/DashboardLayout.js
@@ -1,22 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "../styles/Dashboard.css";
-
-
-
 import Home from "./dashboard/home";
 import Home2 from "./dashboard/home-2";
 import BarChart from "./dashboard/BarChart";
 import DonutChart from "./dashboard/DonutChart";
 
-const DashboardLayout=()=>{
+const DashboardLayout = () => {
 
     return (
-        <Routes>
-            <Route path="/home/index" element={<Home/>} />
-            <Route path="/home/index2" element={<Home2/>} />
-            <Route path="/home/chart2" element={<BarChart/>} />
-            <Route path="/home/dc" element={<DonutChart/>} />
-        </Routes>
+        <div className="font-montserrat">
+            <Routes>
+                <Route path="/home/index" element={<Home />} />
+                <Route path="/home/index2" element={<Home2 />} />
+                <Route path="/home/chart2" element={<BarChart />} />
+                <Route path="/home/dc" element={<DonutChart />} />
+            </Routes>
+        </div>
     );
 }
 
