7ca9fddc10da5f242f0ddda4fdd6130793197976
onjon-deshi <onjon@deshipay.com>
2022-07-28 15:20:35 +0600

    Store Table Update

--- src/components/api/GetInfo.js
+++ src/components/api/GetInfo.js
@@ -1,53 +1,47 @@
 import React, {useEffect, useState} from 'react'
 
 
-// export default function GetInfo() {
-//     const [error, setError] = useState(null);
-//     const [isLoaded, setIsLoaded] = useState(false);
-//     const [apiData, setApiData] = useState([]);
+export default function GetInfo() {
+    
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [apiData, setApiData] = useState([]);
+
+    useEffect(()=>{
+        var getApiData = async () => {
+            await fetch("http://localhost:4000/api").then(res => res.json()).then(
+                (result)=>{
+                    setIsLoaded(true);
+                    setApiData(result);
+                },
+                (error)=>{
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+        }
+
+        getApiData();
+
+    }, []);
 
 
+    if(error) {
+        return (<>Error : {error.message}</>);
+    }
+    else if(isLoaded !== true) {
+        return (<>Loading...</>)
+    }
+    else {
+        return (
+            <div className='py-2'>
+                {apiData.status === undefined ? "No status found" : `Status: ` +  apiData.status }
+                <br/>
+                {apiData.message === undefined ? "No message found." : "Message : " + apiData.message }
+                <br/>
+                {apiData.user === undefined ? "No user found." : "Users Information : " + JSON.stringify(apiData.user) }
+            </div>
+        );
+    }
 
-//     useEffect(()=>{
-//         var getApiData = async () => {
-//             await fetch("http://localhost:4000/api").then(res => res.json()).then(
-//                 (result)=>{
-//                     setIsLoaded(true);
-//                     setApiData(result);
-//                 },
-//                 (error)=>{
-//                     setIsLoaded(true);
-//                     setError(error);
-//                 }
-//             );
-//         }
-
-//         getApiData();
-
-//     }, []);
-
-
-//     if(error) {
-//         return (<>Error : {error.message}</>);
-//     }
-//     else if(isLoaded !== true) {
-//         return (<>Loading...</>)
-//     }
-//     else {
-//         return (
-//             <div className='py-2'>
-//                 {apiData.status === undefined ? "No status found" : `Status: ` +  apiData.status }
-//                 <br/>
-//                 {apiData.message === undefined ? "No message found." : "Message : " + apiData.message }
-//                 <br/>
-//                 {apiData.user === undefined ? "No user found." : "Users Information : " + JSON.stringify(apiData.user) }
-//             </div>
-//         );
-//     }
-
-// }
-
-var GetInfo;
-export default GetInfo = () => {
-    return (<div className='my-3'>API Call Disabled Now.</div>);
 }
